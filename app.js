a4a77edc0bbec05a0e7214f0947436f6d596b440
demir102 <abdutahmanabdutahman8@gmail.com>
2024-07-04 18:38:18 +0300

    test bot

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import apiRoutes from './routes/api.js';
+import { Bot, Context, InlineKeyboard, Keyboard } from "grammy";
+import {writeFile,createWriteStream} from "fs";
+import { Readable } from "stream";
+
+const bot = new Bot("6345823380:AAEtun0-VQyxFCXYBPqtZHw6PK0Rcz-VXbU");
 
 const app = express();
 const port = 3000;
 
-// app.use(express.json());
-// app.use('/api', apiRoutes);
+bot.command("start", (ctx) => ctx.reply("hello user"));
+
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
 app.use('/images',express.static('images'))
@@ -21,5 +25,7 @@ res.sendFile('index.html', {root: '.'})
 app.listen(port, () => {
 
 console.log(`Example app listening on port ${port}`) 
+
+bot.start(); 
 
 });
