bca0fd911d05c2070491d37f4c6476e8431e7e45
demir102 <abdutahmanabdutahman8@gmail.com>
2024-07-06 19:47:57 +0300

    test 1.8

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ import { Bot, Context} from "grammy";
 import {writeFile,createWriteStream} from "fs";
 import { Readable } from "stream";
 
-let bot = new Bot("6345823380:AAEtun0-VQyxFCXYBPqtZHw6PK0Rcz-VXbU");
+let botToken = process.env.BOT_TOKEN
+let bot = new Bot(botToken);
 
 export {bot};
